2845ad32153da14cf92920eec377c6903595e4d4
Stiven Richardy <161863100+Stiven-Richardy@users.noreply.github.com>
2026-03-25 21:46:22 -0300

    Add init database

---
+++ init-database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE aulasistemaweb;
+
+USE aulasistemaweb;
+
+CREATE TABLE user905 (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    password VARCHAR(100),
+    email VARCHAR(100),
+    country VARCHAR(100)
+);
